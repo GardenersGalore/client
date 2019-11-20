dfa3033ecb9701785e74c70dcd4a9642302a9122
Chris Johnston <chris14b@gmail.com>
2019-11-20 19:59:58 +1100

    Styling changes

--- src/views/user.tsx
+++ src/views/user.tsx
@@ -124,18 +124,14 @@ export const UserView: React.FC<UserProps> = (props: UserProps) => {
               <Row className="user-row">
                 <Card title="Recent Blogs">
                   <Timeline>
-                    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-                    <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
-                    <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
-                    <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+                    <Timeline.Item>I planted something!</Timeline.Item>
+                    <Timeline.Item>I planted another thing!</Timeline.Item>
+                    <Timeline.Item>Been planting more things</Timeline.Item>
                   </Timeline>
                 </Card>
               </Row>
             </Col>
             </Row>
-
-
-
         </div>
       );
     }
